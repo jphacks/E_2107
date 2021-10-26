88e6e187f460348751fa3f8fd7af6565f08da5f6
Riku <smile6280@icloud.com>
2021-10-27 03:33:35 +0900

    chamge: モデル定義を変更しました

--- backend/self_introductions/models.py
+++ backend/self_introductions/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # プロフィールのユーザー定義
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
+
+
+class User(AbstractUser):
+    pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     hobby = models.CharField(max_length=50)
-    talent = models.CharField(max_length=50) 
+    talent = models.CharField(max_length=50)
     born = models.CharField(max_length=50)
     job = models.CharField(max_length=50)
     twitter = models.URLField()
@@ -14,5 +20,3 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
-
-
