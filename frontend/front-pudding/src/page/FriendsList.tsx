ba4dcfee1c3076f3dec55efd7d1cd61448c5283b
Kazu0214 <ny3416944@gmail.com>
2021-10-26 17:43:51 +0900

    add 簡単にフレンドリストの見た目を作りました

--- frontend/front-pudding/src/page/FriendsList.tsx
+++ frontend/front-pudding/src/page/FriendsList.tsx
@@ -1,9 +1,205 @@
+import { Button, createTheme, makeStyles } from "@mui/material";
+import { Avatar } from "@mui/material";
+import HiguIcon from "../image/higuSample.jpg";
+import { Container } from "@mui/material";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 
+const name = "山田　太郎"
 
 export default function FriendsList() {
   return (
     <>
-      <p>友達</p>
+    <Container>
+      <Box textAlign="center" fontSize="25px">
+        <h1>友達</h1>
+      </Box> 
+    </Container >
+    <Grid container spacing={2}>
+    <Container>
+    <Box display="flex">
+    <Box marginLeft="5%" marginTop="5%">
+      <Button color="primary" variant="outlined" href="/profile"> 
+
+        <Avatar 
+          alt="UserIcon"
+          src={HiguIcon} 
+          sx={{
+            height: 80,
+            width: 80,
+            fontSize: 70,
+            margin:1
+            }}>
+        </Avatar>
+
+        <Typography
+                component="h2"
+                variant="h2"
+                sx={{
+                  margin: 1,
+                  marginLeft: 5,
+                  marginRight: 5,
+                }}
+              >
+                {name}
+              </Typography>
+      </Button>
+    </Box>
+
+    <Box marginLeft="5%" marginTop="5%" >
+      <Button color="primary" variant="outlined" > 
+
+        <Avatar 
+          alt="UserIcon"
+          src={HiguIcon} 
+          sx={{
+            height: 80,
+            width: 80,
+            fontSize: 70,
+            margin:1
+            }}>
+        </Avatar>
+
+        <Typography
+                component="h2"
+                variant="h2"
+                sx={{
+                  margin: 1,
+                  marginLeft: 5,
+                  marginRight: 5,
+                }}
+              >
+                {name}
+          </Typography>
+      </Button>
+    </Box> 
+    </Box>
+    </Container>
+    </Grid>
+    <Grid container spacing={2}>
+    <Container>
+    <Box display="flex">
+    <Box marginLeft="5%" marginTop="5%">
+      <Button color="primary" variant="outlined"> 
+
+        <Avatar 
+          alt="UserIcon"
+          src={HiguIcon} 
+          sx={{
+            height: 80,
+            width: 80,
+            fontSize: 70,
+            margin:1
+            }}>
+        </Avatar>
+
+        <Typography
+                component="h2"
+                variant="h2"
+                sx={{
+                  margin: 1,
+                  marginLeft: 5,
+                  marginRight: 5,
+                }}
+              >
+                {name}
+              </Typography>
+      </Button>
+    </Box>
+
+    <Box marginLeft="5%" marginTop="5%" >
+      <Button color="primary" variant="outlined" > 
+
+        <Avatar 
+          alt="UserIcon"
+          src={HiguIcon} 
+          sx={{
+            height: 80,
+            width: 80,
+            fontSize: 70,
+            margin:1
+            }}>
+        </Avatar>
+
+        <Typography
+                component="h2"
+                variant="h2"
+                sx={{
+                  margin: 1,
+                  marginLeft: 5,
+                  marginRight: 5,
+                }}
+              >
+                {name}
+          </Typography>
+      </Button>
+    </Box> 
+    </Box>
+    </Container>
+    </Grid>
+    <Grid container spacing={2}>
+    <Container>
+    <Box display="flex">
+    <Box marginLeft="5%" marginTop="5%">
+      <Button color="primary" variant="outlined"> 
+
+        <Avatar 
+          alt="UserIcon"
+          src={HiguIcon} 
+          sx={{
+            height: 80,
+            width: 80,
+            fontSize: 70,
+            margin:1
+            }}>
+        </Avatar>
+
+        <Typography
+                component="h2"
+                variant="h2"
+                sx={{
+                  margin: 1,
+                  marginLeft: 5,
+                  marginRight: 5,
+                }}
+              >
+                {name}
+              </Typography>
+      </Button>
+    </Box>
+
+    <Box marginLeft="5%" marginTop="5%" >
+      <Button color="primary" variant="outlined" > 
+
+        <Avatar 
+          alt="UserIcon"
+          src={HiguIcon} 
+          sx={{
+            height: 80,
+            width: 80,
+            fontSize: 70,
+            margin:1
+            }}>
+        </Avatar>
+
+        <Typography
+                component="h2"
+                variant="h2"
+                sx={{
+                  margin: 1,
+                  marginLeft: 5,
+                  marginRight: 5,
+                }}
+              >
+                {name}
+          </Typography>
+          
+      </Button>
+    </Box> 
+    </Box>
+    </Container>
+    </Grid>
     </>
   );
 }
