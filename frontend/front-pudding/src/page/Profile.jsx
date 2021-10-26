2d24dbc8af5d811b17f11db0fa6c79133b7fa57f
higuchimmy <higutomo.0814.xr@icloud.com>
2021-10-26 22:40:53 +0900

    [fix]項目・SNS連携

--- frontend/front-pudding/src/page/Profile.jsx
+++ frontend/front-pudding/src/page/Profile.jsx
@@ -5,6 +5,7 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
+import Button from '@mui/material/Button';
 import Container from "@mui/material/Container";
 import HiguIcon from "../image/higuSample.jpg";
 import TwitterIcon from "../image/Twitter social icons - circle - blue.png";
@@ -14,8 +15,8 @@ import FaceBookIcon from "../image/f_logo_RGB-Blue_100.png";
 import PropTypes from "prop-types";
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -59,22 +60,43 @@ const useStyles = makeStyles((theme) => ({
 const emails = ["username@gmail.com", "user02@gmail.com"];
 
 function SimpleDialog(props) {
-  const { onClose, selectedValue, open, data, category} = props;
+  const { onClose, selectedValue, open, data, category } = props;
 
   const handleClose = () => {
     onClose(selectedValue);
   };
 
-
   return (
     <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>higuの趣味</DialogTitle>
+      <DialogTitle>
+        {category === "born" && (
+          <DialogContentText>出身</DialogContentText>
+        )}
+        {category === "job" && (
+          <DialogContentText>大学・職場</DialogContentText>
+        )}
+        {category === "hobby" && (
+          <DialogContentText>趣味</DialogContentText>
+        )}
+        {category === "talent" && (
+          <DialogContentText>特技</DialogContentText>
+        )}
+        {!category && <DialogContentText>ありません</DialogContentText>}
+      </DialogTitle>
       <DialogContent>
-      {category === "born" && <DialogContentText>{data.born}</DialogContentText>}
-      {category === "job" && <DialogContentText>{data.job}</DialogContentText>}
-      {category === "hobby" && <DialogContentText>{data.hobby}</DialogContentText>}
-      {category === "talent" && <DialogContentText>{data.talent}</DialogContentText>}
-      {!category  && <DialogContentText>ありません</DialogContentText>}
+        {category === "born" && (
+          <DialogContentText>{data.born}</DialogContentText>
+        )}
+        {category === "job" && (
+          <DialogContentText>{data.job}</DialogContentText>
+        )}
+        {category === "hobby" && (
+          <DialogContentText>{data.hobby}</DialogContentText>
+        )}
+        {category === "talent" && (
+          <DialogContentText>{data.talent}</DialogContentText>
+        )}
+        {!category && <DialogContentText>ありません</DialogContentText>}
         {/* <DialogContentText>{data.born}</DialogContentText>
         <DialogContentText>{data.hobby}</DialogContentText> */}
       </DialogContent>
@@ -114,15 +136,12 @@ export default function Profile() {
           "Content-Type": "application/json",
         },
       })
-      .then(
-        (response) => {
-          // console.log(response.data.filter((user) => user.name === "higu"));
-          console.log(response.data);
-          //一つしか入れてない
-          setData(...response.data);
-
-        }
-      );
+      .then((response) => {
+        // console.log(response.data.filter((user) => user.name === "higu"));
+        console.log(response.data);
+        //一つしか入れてない
+        setData(...response.data);
+      });
   }, []);
 
   return (
@@ -137,21 +156,30 @@ export default function Profile() {
         />
         <Grid item md={4} xs={6} className={classes.bottom}>
           <Container fixed>
-            <Box className={classes.outerCircle} onClick={() => handleClickOpen("born")}>
+            <Box
+              className={classes.outerCircle}
+              onClick={() => handleClickOpen("born")}
+            >
               <Typography variant="h5">出身</Typography>
             </Box>
           </Container>
         </Grid>
         <Grid item md={4} xs={6} className={classes.center}>
           <Container fixed>
-            <Box className={classes.outerCircle} onClick={() => handleClickOpen("job")}>
+            <Box
+              className={classes.outerCircle}
+              onClick={() => handleClickOpen("job")}
+            >
               <Typography variant="h5">大学</Typography>
             </Box>
           </Container>
         </Grid>
         <Grid item md={4} xs={6} className={classes.bottom}>
           <Container fixed>
-            <Box className={classes.outerCircle} onClick={() => handleClickOpen("hobby")}>
+            <Box
+              className={classes.outerCircle}
+              onClick={() => handleClickOpen("hobby")}
+            >
               <Typography variant="h5">趣味</Typography>
             </Box>
           </Container>
@@ -185,8 +213,9 @@ export default function Profile() {
               <Avatar
                 alt="UserIcon"
                 src={HiguIcon}
-                sx={{ width: 130, height: 130 }}
+                sx={{ width: 130, height: 130, marginBottom: 2 }}
               />
+              <Button variant="contained">お気に入りに追加</Button>
               <Box
                 sx={{
                   marginTop: 2,
@@ -196,26 +225,38 @@ export default function Profile() {
                 }}
               >
                 <a
-                  href={`https://github.com/${"higuchimmy"}`}
+                  href={`https://twitter.com/${data.twitter}`}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <Avatar
                     alt="UserIcon"
                     src={TwitterIcon}
-                    sx={{margin: 1,  width: 50, height: 50 }}
+                    sx={{ margin: 1, width: 50, height: 50 }}
                   />
                 </a>
+                <a
+                  href={`https://www.instagram.com/${data.twitter}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                 <Avatar
                   alt="UserIcon"
                   src={InstaIcon}
                   sx={{ margin: 1, width: 50, height: 50 }}
                 />
+                </a>
+                <a
+                  href={`https://github.com/${data.twitter}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                 <Avatar
                   alt="UserIcon"
                   src={FaceBookIcon}
                   sx={{ margin: 1, width: 50, height: 50 }}
                 />
+                </a>
               </Box>
             </Box>
           </Container>
@@ -229,21 +270,30 @@ export default function Profile() {
         </Grid>
         <Grid item md={4} xs={6} className={classes.top}>
           <Container fixed>
-            <Box className={classes.outerCircle} onClick={() => handleClickOpen("")}>
+            <Box
+              className={classes.outerCircle}
+              onClick={() => handleClickOpen("")}
+            >
               <Typography variant="h5">好きな食べ物</Typography>
             </Box>
           </Container>
         </Grid>
         <Grid item md={4} xs={6} className={classes.center}>
           <Container fixed>
-            <Box className={classes.outerCircle} onClick={() => handleClickOpen("")}>
+            <Box
+              className={classes.outerCircle}
+              onClick={() => handleClickOpen("")}
+            >
               <Typography variant="h5">好きな曲</Typography>
             </Box>
           </Container>
         </Grid>
         <Grid item md={4} xs={12} className={classes.top}>
           <Container fixed>
-            <Box className={classes.outerCircle} onClick={() => handleClickOpen("talent")}>
+            <Box
+              className={classes.outerCircle}
+              onClick={() => handleClickOpen("talent")}
+            >
               <Typography variant="h5">マイブーム</Typography>
             </Box>
           </Container>
