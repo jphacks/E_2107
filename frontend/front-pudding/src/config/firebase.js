9552bb6d49bad17f47aa9309f44c25fc6078f8db
Riku <smile6280@icloud.com>
2021-10-28 14:06:19 +0900

    hotfix: FIREBASEのAPIKEYをローカル環境に移行しました。

--- frontend/front-pudding/src/config/firebase.js
+++ frontend/front-pudding/src/config/firebase.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase/compat';
+import * as FIREBASE_CONFIG from '../../.env.local'
 
 firebase.initializeApp({
-	apiKey: "AIzaSyDxYFjyG9NheHbpKo0uxZSw_OkC1z4p8uQ",
-	authDomain: "pj-pudding.firebaseapp.com",
-	projectId: "pj-pudding",
-	storageBucket: "pj-pudding.appspot.com",
-	messagingSenderId: "382465989630",
-	appId: "1:382465989630:web:dc1602b6c7bf9de92a3eaf",
-	measurementId: "G-64J62ZQBV2"
+	apiKey: FIREBASE_CONFIG.REACT_APP_FIREBASE_API_KEY,
+	authDomain: FIREBASE_CONFIG.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: FIREBASE_CONFIG.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: FIREBASE_CONFIG.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: FIREBASE_CONFIG.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+	appId: FIREBASE_CONFIG.REACT_APP_FIREBASE_APP_ID,
+	measurementId: FIREBASE_CONFIG.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 export const auth = firebase.auth();
