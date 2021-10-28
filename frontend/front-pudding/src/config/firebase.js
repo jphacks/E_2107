0cc281c708938c52d2b9de01551864346e65547c
Riku <smile6280@icloud.com>
2021-10-28 18:33:43 +0900

    add: firebaseCLI

--- frontend/front-pudding/src/config/firebase.js
+++ frontend/front-pudding/src/config/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase/compat';
-import * as FIREBASE_CONFIG from '../../.env.local'
 
 firebase.initializeApp({
-	apiKey: FIREBASE_CONFIG.REACT_APP_FIREBASE_API_KEY,
-	authDomain: FIREBASE_CONFIG.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: FIREBASE_CONFIG.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: FIREBASE_CONFIG.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: FIREBASE_CONFIG.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-	appId: FIREBASE_CONFIG.REACT_APP_FIREBASE_APP_ID,
-	measurementId: FIREBASE_CONFIG.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 export const auth = firebase.auth();
