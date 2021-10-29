daf0b9a1c03f602a36fa05640e16408ba6e441cc
Riku <smile6280@icloud.com>
2021-10-30 03:20:40 +0900

    fix: import Reactを追加しました

--- frontend/front-pudding/src/components/PrivateRoute.js
+++ frontend/front-pudding/src/components/PrivateRoute.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Route, Redirect } from 'react-router-dom';
 import { useAuthContext } from '../authContext';
 const PrivateRoute = ({ component: Component, ...rest }) => {
