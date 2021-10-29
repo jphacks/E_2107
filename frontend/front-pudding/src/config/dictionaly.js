1791152c671f469559b8fdb42157d15bd99b6c5d
Riku <smile6280@icloud.com>
2021-10-30 06:45:59 +0900

    add: dictファイルの追加

---
+++ frontend/front-pudding/src/config/dictionaly.js
@@ -0,0 +1,9 @@
+var categoryDictionaly = [
+    {key: "born", name: "出身地"},
+    {key: "job", name: "大学・職場"},
+    {key: "hobby", name: "趣味"},
+    {key: "favorite_food", name: "好きな食べ物"},
+    {key: "dream", name: "夢"},
+    {key: "talent", name: "特技"},
+]
+export default categoryDictionaly;
